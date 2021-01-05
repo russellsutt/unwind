0689da27392fa3a62df823a31d25a07effa68882
russellsutt <66880560+russellsutt@users.noreply.github.com>
2021-01-05 01:13:17 -0800

    Update unwind-the-coil.js

--- unwind-the-coil.js
+++ unwind-the-coil.js
@@ -1,4 +1,5 @@
 // You have a coiled snake matrix.  Look at the numerical order to understand the pattern that must be returned.
+// This must be scalable so it is not limited to a 4x4 matrix.
 
 const snakeMatrix = [
     [ 1, 2, 3, 4 ],
@@ -16,7 +17,7 @@ const snakeMatrix = [
 
 // You can return segments in different lines.  But they must not be returned in an array.  (Check the spread operator)
 
-// What do you do when you complete a full circle? Recursion!
+// Can we use recusion?
 
 // Have fun!
 
