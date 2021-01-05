aa02eec74b7e227d0a1b1ce3c29240cf8681e17e
russellsutt <russell.sutter@gmail.com>
2021-01-05 00:08:17 -0800

    Solve it

--- unwind-the-coil.js
+++ unwind-the-coil.js
@@ -1,19 +1,11 @@
-// You have a coiled snake matrix.  Look at the numerical order to understand the pattern.
+// You have a coiled snake matrix.  Look at the numerical order to understand the pattern that must be returned.
 
 const snakeMatrix = [
     [1, 2, 3, 4],
     [ 12, 13, 14, 5],
-    [ 11, 16, 15, 6]
+    [ 11, 16, 15, 6],
     [ 10, 9, 8, 7]
 ]
-
-
-unwind = (matrix) => {
-    // your code here
-}
-
-
-unwind(snakeMatrix)
 
 // expected return 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16.
 
@@ -24,3 +16,12 @@ unwind(snakeMatrix)
 // You can return segments in different lines.  But they must not be returned in an array.  (Check the spread operator)
 
 // Have fun!
+
+
+unwind = (matrix) => {
+    // your code here
+}
+
+
+unwind(snakeMatrix)
+
